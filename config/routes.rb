bf14cb102fe573ce66a2b66e96ff263a8546a08c
makicamel <unright@gmail.com>
2024-12-14 19:32:52 +0900

    Add a route for erd_map

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 ErdMap::Engine.routes.draw do
+  root to: "erd_map#index"
 end
--- spec/fake_app/config/routes.rb
+++ spec/fake_app/config/routes.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 ErdMapTestApp::Application.routes.draw do
-  mount ErdMap::Engine => "/erd_map"
 end
