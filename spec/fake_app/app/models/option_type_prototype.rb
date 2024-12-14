aeae16fac964068147cdcd21d6486ef632e85b81
makicamel <unright@gmail.com>
2024-12-14 17:30:34 +0900

    Add fake_app models

---
+++ spec/fake_app/app/models/option_type_prototype.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Models from spree
+# https://github.com/spree/spree
+class OptionTypePrototype < ApplicationRecord
+end
---
+++ spec/fake_app/app/models/option_value.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Models from spree
+# https://github.com/spree/spree
+class OptionValue < ApplicationRecord
+end
