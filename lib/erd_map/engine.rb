03b2caa67c89eb5c62ba9140557af662f9408b30
makicamel <unright@gmail.com>
2025-01-15 17:52:15 +0900

    Require explicit mount for ease to custom
    For example, adding authentication.

--- lib/erd_map/engine.rb
+++ lib/erd_map/engine.rb
@@ -1,13 +1,5 @@
 module ErdMap
   class Engine < ::Rails::Engine
     isolate_namespace ErdMap
-
-    initializer "erd_map" do |app|
-      ActiveSupport.on_load :after_initialize do
-        Rails.application.routes.prepend do
-          mount ErdMap::Engine, at: "/erd_map"
-        end
-      end
-    end
   end
 end
--- spec/fake_app/config/routes.rb
+++ spec/fake_app/config/routes.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 ErdMapTestApp::Application.routes.draw do
+  mount ErdMap::Engine => "/erd_map"
 end
